3ef4d739ed11fb27babd3d7b0efe8cdab149f0f4
shailaja <SHA056@github.com>
2020-08-11 15:45:27 +0530

    errors package

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,12 +1,11 @@
 package users
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/SHA056/bookstore_users/domain/users"
 	"github.com/SHA056/bookstore_users/services"
+	"github.com/SHA056/bookstore_users/utils/errors"
 	"github.com/gin-gonic/gin"
-	"io/ioutil"
 	"net/http"
 )
 
@@ -14,29 +13,43 @@ func CreateUser(c *gin.Context) {
 	var user users.User
 	fmt.Println(user)
 
-	// Any request that comes through gets saved into the bytes object
-	bytes, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil {
-		// Handle error
-		//fmt.Println(err)
+	//// Any request that comes through gets saved into the bytes object
+	//bytes, err := ioutil.ReadAll(c.Request.Body)
+	//if err != nil {
+	//	// Handle error
+	//	//fmt.Println(err)
+	//	return
+	//}
+	//fmt.Println("MARK 1>>>>>  reading bytes completed")
+	//
+	//// Using the bytes object to fill the details into another structure
+	////json.Unmarshal(bytes, &user)
+	//
+	//if err := json.Unmarshal(bytes, &user) ; err != nil {
+	//		// Handle error
+	//		fmt.Println(err.Error())
+	//		return
+	//}
+
+	// Basically does all of the above in a single line of code
+	if err := c.ShouldBindJSON(&user); err != nil {
+		// Handle JSON error
+		restErr := errors.NewBadRequestError("invalid  json body")
+		c.JSON(restErr.Status, restErr)
+		fmt.Println(err)
 		return
 	}
-	fmt.Println("MARK 1>>>>>  reading bytes completed")
 
-	// Using the bytes object to fill the details into another structure
-	//json.Unmarshal(bytes, &user)
-
-	if err := json.Unmarshal(bytes, &user) ; err != nil {
-			// Handle error
-			fmt.Println(err.Error())
-			return
-	}
 	fmt.Println("MARK 2>>>>> Marshalling completed")
 	//fmt.Println(string(bytes))
+
+	// Sending to service for processing
+	fmt.Println("Sending to service for processing")
 	result, saveError := services.CreateUser(user)
 	fmt.Println("MARK 3>>>>> result saved")
 	if saveError != nil {
 		// Handle Error
+		c.JSON(saveError.Status,saveError)
 		fmt.Println(saveError)
 		return
 	}
--- services/users_service.go
+++ services/users_service.go
@@ -1,9 +1,12 @@
 package services
 
-import "github.com/SHA056/bookstore_users/domain/users"
+import (
+	"github.com/SHA056/bookstore_users/domain/users"
+	"github.com/SHA056/bookstore_users/utils/errors"
+)
 
 //  Holds the entire business logic
 
-func CreateUser(user users.User) (*users.User, error) {
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	return &user,nil
 }
